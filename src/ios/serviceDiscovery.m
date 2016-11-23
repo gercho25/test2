2354996acbf7e1ad91df05a10a88c6c28bbb62d4
gercho25 <germanesalinas@gmail.com>
2016-11-23 10:27:00 -0300

    Cambios

--- src/ios/serviceDiscovery.m
+++ src/ios/serviceDiscovery.m
@@ -55,8 +55,15 @@ NSMutableArray *serviceArr;
                     broadcastAddr.sin_port = htons(1900);
 
                     // Send the broadcast request for the given service type
-                    NSString *request = [[@"M-SEARCH * HTTP/1.1\r\nHOST: 239.255.255.250:1900\r\nMAN: \"ssdp:discover\"\r\nST: " stringByAppendingString:serviceType] stringByAppendingString:@"\"\r\nMX: 2\r\n\r\n"];
-                    char *requestStr = [request UTF8String];
+                    
+					//NSString *request = [[@"M-SEARCH * HTTP/1.1\r\nHOST: 239.255.255.250:1900\r\nMAN: \"ssdp:discover\"\r\nST: " stringByAppendingString:serviceType] stringByAppendingString:@"\"\r\nMX: 2\r\n\r\n"];
+                    
+					serviceType = @"ssdp:all";
+					NSString *request = @"M-SEARCH * HTTP/1.1\r\nHOST: 239.255.255.250:1900\r\nMAN: \"ssdp:discover\"\r\nST: ";
+					request = [request stringByAppendingFormat:	@"%@", serviceType];
+					request = [request stringByAppendingFormat:	@"\r\nMX: 2\r\n\r\n"];
+					
+					char *requestStr = [request UTF8String];
 
                     ret = sendto(sd, requestStr, strlen(requestStr), 0, (struct sockaddr*)&broadcastAddr, sizeof broadcastAddr);
                     if (ret < 0) {
